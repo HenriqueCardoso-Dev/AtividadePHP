f2f5b28cc7ef8c7cce69aa7db87bccdd452fc50e
HenriqueCardoso-Dev <carloshenriqueramoscardoso@gmail.com>
2021-02-26 23:32:34 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    
+        <label for="username">Username:</label>
+        <input type="text" name="user" id="username">
+       
+        <label for="pass">Password:</label>
+        <input type="password" name="pass" id="pass">
+
+        <input type="submit" value="Conect">
+
+    </form>
+</body>
+</html>

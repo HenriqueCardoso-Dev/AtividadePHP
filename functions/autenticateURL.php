13bdda149ecd3db0fcba6664609ad787922adec4
HenriqueCardoso-Dev <carloshenriqueramoscardoso@gmail.com>
2021-03-01 18:55:44 -0300

    create-youtube-player

---
+++ functions/autenticateURL.php
@@ -0,0 +1,28 @@
+<?php
+
+    function autenticateURL($url) {
+        
+
+        $verifYTB = explode('.', $url);
+
+        switch ($verifYTB[1]) {
+            case 'youtube':
+                # code...
+                $videoYTB = explode('=', $url);
+
+                $ifIsPlaylistOrNot = explode('&', $videoYTB[1]);
+
+                $embedPlayer = "<iframe width='560' height='315' src='https://www.youtube.com/embed/$ifIsPlaylistOrNot[0]' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+
+                return $embedPlayer;
+            break;
+            default:
+                # code...
+                $embedPlayer = "Desculpa, mas só aceitamos links do <strong>YouTube</strong>.";
+                return $embedPlayer;
+            break;
+        }
+
+    }
+
+?>
--- pages/youtubeEmbedPlayer/index.php
+++ pages/youtubeEmbedPlayer/index.php
@@ -1,5 +1,57 @@
-<?php
-    session_start();
 
-    echo $_SESSION["user"];
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+
+    <link rel="stylesheet" href="style.scss">
+
+    <title>Youtube Player</title>
+</head>
+<body>
+
+    <section class="hero is-primary is-fullheight is-bold">
+        <div class="hero-body container has-text-centered form-player">
+            <form action="" method="POST">
+                <?php
+                    session_start();
+
+                    echo "<label for='link' class='label'>".$_SESSION["user"].", insert a Youtube URL</label>";
+                ?>
+                
+                <input type="url" name="linkValue" id="link" class="input" placeholder="URL: https://www.example.com">
+
+                <input type="submit" value="Play" class="button is-danger">
+
+            </form>
+
+
+            <div class="playerField">
+            <?php
+    
+                if (isset($_POST['linkValue'])) {
+                    
+                    $url = $_POST['linkValue'];
+
+                    require_once "../../functions/autenticateURL.php";
+
+                    echo autenticateURL($url);
+
+                }
+            
+            
+            ?>
+            </div>
+
+        </div>
+    </section>
+
+    
+    
+</body>
+</html>
